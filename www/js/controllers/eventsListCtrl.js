3bf26e1b1d6ab64024f96176f5c50d0dafe93f71
Magesh K <mgshk.1507@gmail.com>
2016-04-20 23:43:45 +0530

    updated events list and details

--- www/js/controllers/eventsListCtrl.js
+++ www/js/controllers/eventsListCtrl.js
@@ -1,5 +1,8 @@
 angular.module('eventsApp.controllers.eventsListCtrl', [])
-	.controller('eventsListCtrl', ['$scope', function($scope) {
+    .controller('eventsListCtrl', ['$scope', '$state', 'eventsFactory', function($scope, $state, eventsFactory) {
 	
-	
+    eventsFactory.getEventsList().then(function (resp) {
+        $scope.events = resp.data;
+        console.log(resp);
+    });
 }]);
