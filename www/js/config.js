6333f3839d7f6d481bf4244bcbf4a98a44b7f070
Nagamurali <nagamuraliweb@gmail.com>
2016-04-29 00:14:21 +0530

    conflict resolved

--- www/js/config.js
+++ www/js/config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 angular.module('eventsApp.config', ['ui.router']).config(function($stateProvider, $urlRouterProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
@@ -68,73 +67,4 @@ angular.module('eventsApp.config', ['ui.router']).config(function($stateProvider
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/');
 
-=======
-angular.module('eventsApp.config', ['ui.router']).config(function($stateProvider, $urlRouterProvider, $facebookProvider) {
-    
-    $facebookProvider.setAppId('1677847679144225');
-    
-    $stateProvider
-    .state('home', {
-        url: '/',
-        controller: 'homeCtrl',
-        templateUrl: 'templates/home.html',
-        isRequiredLogin: false
-    })
-    .state('login', {
-        url: '/login',
-        controller: 'loginCtrl',
-        templateUrl: 'templates/login.html',
-        isRequiredLogin: false
-    })
-    .state('signup', {
-        url: '/signup',
-        controller: 'signupCtrl',
-        templateUrl: 'templates/signup.html',
-        isRequiredLogin: false
-    })
-    .state('forgotPassword', {
-        url: '/forgotPassword',
-        controller: 'forgotPasswordCtrl',
-        templateUrl: 'templates/forgotPassword.html',
-        isRequiredLogin: false
-    })    
-    .state('eventsList', {
-        url: '/eventsList',
-        controller: 'eventsListCtrl',
-        templateUrl: 'templates/eventsList.html',
-        isRequiredLogin: true
-    })
-    .state('eventDetails', {
-        url: '/eventDetails/:event_id',
-        controller: 'eventDetailsCtrl',
-        templateUrl: 'templates/eventDetails.html',
-        isRequiredLogin: true
-    })
-    .state('filterEvents', {
-        url: '/filterEvents/:address/:start_date',
-        controller: 'filterEventsCtrl',
-        templateUrl: 'templates/filterEvents.html',
-        isRequiredLogin: true
-    })
-    .state('yourBooking', {
-        url: '/yourBooking',
-        controller: 'yourBookingCtrl',
-        templateUrl: 'templates/yourBooking.html',
-        isRequiredLogin: true
-    })
-    .state('bookingSubmission', {
-        url: '/bookingSubmission',
-        controller: 'bookingSubmissionCtrl',
-        templateUrl: 'templates/bookingSubmission.html',
-        isRequiredLogin: true
-    })
-    .state('payment', {
-        url: '/payment',
-        controller: 'paymentCtrl',
-        templateUrl: 'templates/payment.html',
-        isRequiredLogin: true
-    });
-    
-    $urlRouterProvider.otherwise('/login');
->>>>>>> babf5e577322d99a5e30c3519ac5c980259eeb37
 });
