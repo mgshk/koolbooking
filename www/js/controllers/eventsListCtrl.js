9664733540caa515bb3ac58c869d7eb591db373e
Nagamurali <nagamuraliweb@gmail.com>
2016-05-04 22:44:31 +0530

    changes done

--- www/js/controllers/eventsListCtrl.js
+++ www/js/controllers/eventsListCtrl.js
@@ -5,6 +5,8 @@ angular.module('eventsApp.controllers.eventsListCtrl', [])
 	$scope.tickets = false;
 	$scope.you = false;
 	$scope.discover = false;
+	$scope.trending = false;
+	$scope.comingsoon = false;
 	$('.tab1').css('border-bottom', 'solid 4px #8c9ef2');
 	$('.tab2').css('border-bottom', 'solid 0px #8c9ef2');
 	$('.tab3').css('border-bottom', 'solid 0px #8c9ef2');
@@ -48,10 +50,41 @@ angular.module('eventsApp.controllers.eventsListCtrl', [])
 		$scope.tickets = false;
 		$scope.you = false;
 		$scope.discover = true;
+		$scope.recommended = true;
+		$scope.trending = false;
+		$scope.comingsoon = false;
 		$('.tab1').css('border-bottom', 'solid 0px #8c9ef2');
 		$('.tab2').css('border-bottom', 'solid 0px #8c9ef2');
 		$('.tab3').css('border-bottom', 'solid 0px #8c9ef2');
 		$('.tab4').css('border-bottom', 'solid 4px #8c9ef2');
+		$('.tab5').css('border-top', 'solid 4px #8c9ef2');
+	}
+
+	$scope.showRecommened = function(){
+		$scope.recommended = true;
+		$scope.trending = false;
+		$scope.comingsoon = false;
+		$('.tab5').css('border-top', 'solid 4px #8c9ef2');
+		$('.tab6').css('border-top', 'solid 0px #8c9ef2');
+		$('.tab7').css('border-top', 'solid 0px #8c9ef2');
+	}
+
+	$scope.showTrending = function(){
+		$scope.recommended = false;
+		$scope.trending = true;
+		$scope.comingsoon = false;
+		$('.tab5').css('border-top', 'solid 0px #8c9ef2');
+		$('.tab6').css('border-top', 'solid 4px #8c9ef2');
+		$('.tab7').css('border-top', 'solid 0px #8c9ef2');
+	}
+
+	$scope.showComingsoon = function(){
+		$scope.recommended = false;
+		$scope.trending = false;
+		$scope.comingsoon = true;
+		$('.tab5').css('border-top', 'solid 0px #8c9ef2');
+		$('.tab6').css('border-top', 'solid 0px #8c9ef2');
+		$('.tab7').css('border-top', 'solid 4px #8c9ef2');
 	}
 
     eventsFactory.getEventsList().then(function (resp) {
